8a7ff675af8d2882cb532f2be7365b2845a937d3
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2020-04-19 17:03:18 +0200

    Make Clion happy when parsing filesystem.hpp

--- include/aw/util/filesystem/filesystem.hpp
+++ include/aw/util/filesystem/filesystem.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
-#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
+#if defined(__cplusplus) && __cplusplus >= 201703L
+#define GHC_USE_STD_FS
+#elif defined(__has_include)
 #if __has_include(<filesystem>)
 #define GHC_USE_STD_FS
+#endif
+#endif
+
+#ifdef GHC_USE_STD_FS
 #include <filesystem>
 namespace aw {
 namespace fs = std::filesystem;
 }
-#endif
-#endif
-#ifndef GHC_USE_STD_FS
+#else
 #include <ghc/filesystem.hpp>
 namespace aw {
 namespace aw::fs = ghc::filesystem;
